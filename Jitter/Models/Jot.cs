7413805eacb56712ad6e71d31d88c05e232fedf8
alwadejr <lewis@mrwade.net>
2015-11-18 12:00:58 -0600

    Jot models with Trivial tests

---
+++ Jitter/Models/Jot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jitter.Models
+{
+    public class Jot
+    {
+        public object Author { get; set; }
+        public string Content { get; set; }
+        public DateTime Date { get; set; }
+        public int JotId { get; set; }
+        public string Picture { get; set; }
+    }
+}
