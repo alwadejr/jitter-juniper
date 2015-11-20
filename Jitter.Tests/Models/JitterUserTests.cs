42a0e2457e76595e6012c3bc73ca32deae40eacb
alwadejr <lewis@mrwade.net>
2015-11-20 10:14:23 -0600

    Nov 19th

---
+++ Jitter.Tests/Models/JitterUserTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jitter.Models;
+
+namespace Jitter.Tests
+{
+    [TestClass]
+    public class JitterUsersTests
+    {
+        [TestMethod]
+        public void JitterUserEnsureICanCreateInstance()
+        {
+            JitterUser a_user = new JitterUser();
+            Assert.IsNotNull(a_user);
+        }
+
+        [TestMethod]
+        public void JitterUserEnsureJitterUserHasAllTheThings()
+        {
+            JitterUser a_user = new JitterUser();
+
+            a_user.Handle = "adam1";
+            a_user.FirstName = "Adam";
+            a_user.LastName = "Sandler";
+            a_user.Picture = "https://google.com";
+            a_user.Description = "I am awesome!";
+
+
+            Assert.AreEqual("adam1", a_user.Handle);
+            Assert.AreEqual("Adam", a_user.FirstName);
+            Assert.AreEqual("Sandler", a_user.LastName);
+            Assert.AreEqual("https://google.com", a_user.Picture);
+            Assert.AreEqual("I am awesome!", a_user.Description);
+
+
+        }
+    }
+}       
